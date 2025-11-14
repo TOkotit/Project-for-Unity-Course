e2c920e10dec98a930e30f66f2712a598a51f3f7
zyuzyakin <zyuzyakinIvan06@gmail.com>
2025-11-14 15:37:26 +0500

    Простая турель

---
+++ Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float health = 100f;
+    
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+}
