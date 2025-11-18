242ab061e6da0163a3b3b54baa28bb4bdef4c49d
neograne <neograneforlife@hotmail.com>
2025-11-18 23:13:58 +0500

    added basic en localization

--- Game/Assets/Scripts/UI scripts/SettingsMenu.cs
+++ Game/Assets/Scripts/UI scripts/SettingsMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
@@ -25,7 +26,7 @@ public class SettingsMenu : MonoBehaviour
 
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRateRatio + "Ãö";
+            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRateRatio + "Hz";
             options.Add(option);
             if ((resolutions[i].width == Screen.currentResolution.width) && (resolutions[i].height == Screen.currentResolution.height))
                 currentResolutionIndex = i;
