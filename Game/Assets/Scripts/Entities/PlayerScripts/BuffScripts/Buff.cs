33ef299d02e033f2a43e600747a036f5354ac8f1
zyuzyakin <zyuzyakinIvan06@gmail.com>
2025-12-19 00:18:46 +0500

    начало системы баффов

---
+++ Game/Assets/Scripts/Entities/PlayerScripts/BuffScripts/Buff.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Buff
+{
+    private string name;
+    private string description;
+    private int rarity;
+    
+    private float value;
+    private BuffType type;
+}
---
+++ Game/Assets/Scripts/Entities/PlayerScripts/BuffScripts/BuffSystemModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffSystemModel
+{
+    private List<Buff> buffs;
+    
+    
+}
