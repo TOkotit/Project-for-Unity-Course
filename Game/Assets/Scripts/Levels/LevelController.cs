18417a878e4176a4c7d64dd6d3144cd50231b4ee
TOkotit <105845895+TOkotit@users.noreply.github.com>
2025-12-23 20:18:46 +0500

    через бут можно полноценно поиграть

--- Game/Assets/Scripts/Levels/LevelController.cs
+++ Game/Assets/Scripts/Levels/LevelController.cs
@@ -31,8 +31,6 @@ namespace Levels
 
         public void Awake()
         {
-            _levelModel.LevelCompleted.AddListener(OnLevelCompleted); //+
-
             if (LevelStats == null)
                 LevelStats = Resources.Load<LevelStats00>("Config/Level1StatsSO");
             if (CarEnemyStats == null)
@@ -51,6 +49,7 @@ namespace Levels
                                "Проверьте Script Execution Order для GameplayEntryPoint.");
                 return;
             }
+            _levelModel.LevelCompleted.AddListener(OnLevelCompleted); //+
             Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
             
             SpawnNextWave();
